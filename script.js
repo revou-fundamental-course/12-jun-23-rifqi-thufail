2382715c2bf7307d7980ebddac11e41a57807904
rifqi-thufail <rif2kiki@gmail.com>
2023-06-22 17:26:24 +0700

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-//Value from user to variables is JS
-let suhuCelcius = document.getElementById("inputCelcius");
-let suhuFahrenheit = document.getElementById("inputFahrenheit");
-let calcFahrenheit;
-//Calculate Celcius to Fahrenheit
-function calculateFahrenheit(suhuFahrenheit) {
-  calcFahrenheit = suhuCelcius * 1.8 + 32;
+// Calculate Celsius from Fahrenheit
+function calculateCelsius() {
+  let suhuFahrenheit = document.getElementById("inputFahrenheit").value;
+  let suhuCelcius = (suhuFahrenheit - 32) / 1.8;
+  document.getElementById("inputCelcius").value = suhuFahrenheit;
+}
 
-  document.getElementsByName("inputFahrenheit")[0].placeholder = calcFahrenheit;
+// Calculate Fahrenheit from Celsius
+function calculateFahrenheit() {
+  let suhuCelcius = document.getElementById("inputCelcius").value;
+  let suhuFahrenheit = suhuCelcius * 1.8 + 32;
+  document.getElementById("inputFahrenheit").value = suhuFahrenheit;
+}
+
+//Button Clicked
+function convertButtonClick() {
+  if (document.getElementById("inputCelcius").value !== "") {
+    calculateFahrenheit();
+  } else if (document.getElementById("inputFahrenheit").value !== "") {
+    calculateCelsius();
+  } else {
+    alert("Hello! I am an alert box!!");
+  }
 }
