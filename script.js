c0dd967b3689be5b1c7ce8fff4e0d70d23fa8c37
rifqi-thufail <rif2kiki@gmail.com>
2023-06-22 21:39:57 +0700

    UI additions

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-// Calculate Celsius from Fahrenheit
+// Function to calculate celcius from fahrenheit
 function calculateCelsius() {
-  let suhuFahrenheit = document.getElementById("inputFahrenheit").value;
-  let suhuCelcius = (suhuFahrenheit - 32) / 1.8;
+  var suhuFahrenheit = document.getElementById("inputFahrenheit").value;
+  var suhuCelcius = (suhuFahrenheit - 32) / 1.8;
   document.getElementById("inputCelcius").value = suhuFahrenheit;
 }
 
-// Calculate Fahrenheit from Celsius
+// Function to calculate fahrenheit from celcius
 function calculateFahrenheit() {
-  let suhuCelcius = document.getElementById("inputCelcius").value;
-  let suhuFahrenheit = suhuCelcius * 1.8 + 32;
+  var suhuCelcius = document.getElementById("inputCelcius").value;
+  var suhuFahrenheit = suhuCelcius * 1.8 + 32;
   document.getElementById("inputFahrenheit").value = suhuFahrenheit;
 }
 
-//Button Clicked
+// Convert Button Clicked
 function convertButtonClick() {
   if (document.getElementById("inputCelcius").value !== "") {
     calculateFahrenheit();
   } else if (document.getElementById("inputFahrenheit").value !== "") {
     calculateCelsius();
   } else {
-    alert("Hello! I am an alert box!!");
+    alert("Masukkan Suhu!");
   }
+}
+
+// Reset Button Clicked
+function resetBtnClick() {
+  document.getElementById("inputCelcius").value = "";
+  document.getElementById("inputFahrenheit").value = "";
+  document.getElementById("tempEquation").value = "";
+}
+
+// Reverse Button
+function reverseBtn() {
+  resetBtnClick();
+  for (i = 0; i < document.styleSheets.length; i++) {
+    void (document.styleSheets.item(i).disabled = true);
+  }
+}
+
+// Display the formula used in the converter.
+function displayEquation() {
+  var suhuFahrenheit = document.getElementById("inputFahrenheit").value;
+  var suhuCelcius = document.getElementById("inputCelcius").value;
+  document.getElementById(
+    "tempEquation"
+  ).value = `${suhuCelcius.toString()} * 1,8 + 32`;
 }
